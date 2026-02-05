0cabef652acad76c16ed34ba4e8e138eaac30ab6
Shreya Mishra <shreyamishra2k05@gmail.com>
2026-02-05 19:56:18 +0530

    Time: 11 ms (66.41%), Space: 35.2 MB (18.86%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -1,5 +1,13 @@
 class Solution {
 public:
+    int max(vector<int>& weights, int n){
+        int m=weights[0];
+        for(int i=1; i<n; i++){
+            if(weights[i]>m)
+            m=weights[i];
+        }
+        return m;
+    }
     int totDays(vector<int>& weights, int cap, int n){
         int load=0, day=1;
         for(int i=0; i<n; i++){
@@ -15,15 +23,21 @@ public:
     }
     int shipWithinDays(vector<int>& weights, int days) {
         int n = weights.size();
-        int minC = *max_element(weights.begin(), weights.end());
-        int maxC = accumulate(weights.begin(), weights.end(), 0);
+        int minC = max(weights, n);
+        int sum=0;
+        for(int i=0; i<n;i++){
+            sum = sum+weights[i];
+        }
+        int maxC = sum;
+        int cap=maxC;
         while(minC<=maxC){
            int mid = (minC+maxC)/2;
            int reqDays = totDays(weights, mid, n);
            if(reqDays<=days){
+           cap=mid;
            maxC=mid-1;
         } else minC=mid+1;
         }
-        return minC;  
+        return cap;  
     }
 };
