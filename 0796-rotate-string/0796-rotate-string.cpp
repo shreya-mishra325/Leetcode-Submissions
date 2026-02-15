93422ec98760b6f9f913556f7f035daae28cd58f
Shreya Mishra <shreyamishra2k05@gmail.com>
2026-02-15 23:11:16 +0530

    Time: 0 ms (100%), Space: 8.1 MB (84.21%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -2,11 +2,7 @@ class Solution {
 public:
     bool rotateString(string s, string goal) {
         if(s.size()!=goal.size()) return false;
-        for(int i=0; i<s.size(); i++){
-            s.push_back(s[0]);
-            s.erase(s.begin());
-            if(s==goal) return true;
-        }
-        return false;
+        string doubleS = s+s;
+        return doubleS.find(goal)!= string::npos;
     }
 };
