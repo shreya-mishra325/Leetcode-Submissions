db2bc35a91468fe502d93b4b9cae5e451310cb12
Shreya Mishra <shreyamishra2k05@gmail.com>
2026-04-19 18:59:20 +0530

    Time: 48 ms (37.29%), Space: 88.7 MB (15.14%) - LeetHub

---
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        map<int, int> mpp;
+        int r=0, l=0;
+        int ans=0;
+        while(r<fruits.size()){
+            mpp[fruits[r]]++;
+            while(mpp.size()>2){
+                mpp[fruits[l]]--;
+                if(mpp[fruits[l]]==0)
+                    mpp.erase(fruits[l]);
+                l++;
+            }  
+            ans = max(ans, r-l+1); 
+            r++;
+        }
+        return ans;
+    }
+};
