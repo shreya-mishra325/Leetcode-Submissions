ce7870546c5204fd06f5acad15b7e132902362bc
Shreya Mishra <shreyamishra2k05@gmail.com>
2026-06-29 20:24:04 +0530

    Time: 0 ms (100%), Space: 19.6 MB (8.9%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -11,28 +11,24 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        if(list1==NULL && list2==NULL) return NULL;
-        if(list1==NULL) return list2;
-        if(list2==NULL) return list1;
-        ListNode* h1=list1;
-        ListNode* h2=list2;
-        vector<int> vec;
-        while(h1!=NULL){
-            vec.push_back(h1->val);
-            h1=h1->next;
+        ListNode* t1=list1;
+        ListNode* t2=list2;
+        ListNode* dummy= new ListNode(-1);
+        ListNode* temp = dummy;
+        while(t1!=NULL && t2!=NULL){
+            if(t1->val<t2->val){
+                temp->next=t1;
+                temp=t1;
+                t1=t1->next;
+            }
+            else {
+                temp->next=t2;
+                temp=t2;
+                t2=t2->next;
+            }   
         }
-        while(h2!=NULL){
-            vec.push_back(h2->val);
-            h2=h2->next;
-        }
-        sort(vec.begin(), vec.end());
-        ListNode* h3= new ListNode(vec[0]);
-        ListNode* prev=h3;
-        for(int i=1; i<vec.size(); i++){
-            ListNode* naya = new ListNode(vec[i]);
-            prev->next=naya;
-            prev=naya;
-        }
-        return h3;
+        if(t1) temp->next=t1;
+        else temp->next=t2;
+        return dummy->next;
     }
 };
