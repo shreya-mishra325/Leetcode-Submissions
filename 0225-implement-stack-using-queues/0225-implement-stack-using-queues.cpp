c187367c31507611bdad576903d5f0ac27788de2
Shreya Mishra <shreyamishra2k05@gmail.com>
2026-07-01 23:59:39 +0530

    Time: 0 ms (100%), Space: 10.2 MB (5.09%) - LeetHub

---
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
@@ -0,0 +1,43 @@
+class MyStack {
+public:
+    queue<int> q1;
+    queue<int> q2;
+    MyStack() {
+            
+    }
+    
+    void push(int x) {
+        q1.push(x);
+    }
+    
+    int pop() {
+        // Move all except the last element to q2
+        while (q1.size() > 1) {
+            q2.push(q1.front());
+            q1.pop();
+        }
+        int ans = q1.front();
+        q1.pop();
+
+
+        swap(q1, q2);
+
+        return ans;
+    }
+
+    int top() {
+        while (q1.size() > 1) {
+            q2.push(q1.front());
+            q1.pop();
+        }
+        int ans = q1.front();
+        q2.push(ans);
+        q1.pop();
+        swap(q1, q2);
+        return ans;
+    }
+
+    bool empty() {
+        return q1.empty();
+    }
+};
