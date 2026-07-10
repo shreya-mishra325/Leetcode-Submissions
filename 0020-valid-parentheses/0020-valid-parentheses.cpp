f048c864d324767c92a0d96840a54941ed02276a
Shreya Mishra <shreyamishra2k05@gmail.com>
2026-07-10 23:34:38 +0530

    Time: 0 ms (100%), Space: 8.7 MB (89.45%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,19 +1,24 @@
 class Solution {
 public:
     bool isValid(string s) {
-        unordered_map<char, char> mpp;
-        mpp[')'] = '('; mpp[']'] = '['; mpp['}']='{';
         stack<char> st;
         for(int i=0; i<s.size(); i++){
-            if(s[i]=='(' || s[i]=='[' || s[i]=='{'){
+            if(s[i]=='(' || s[i]=='{' || s[i]=='['){
                 st.push(s[i]);
-            } else {
-                if(st.empty() || st.top()!=mpp[s[i]]){
-                    return false;
-                }
+            }
+            else if(s[i]==')'){
+                if(st.empty() || st.top()!='(') return false;
+                st.pop();
+            }
+            else if(s[i]=='}'){
+                if(st.empty() || st.top()!='{') return false;
+                st.pop();
+            }
+            else if(s[i]==']'){
+                if(st.empty() || st.top()!='[') return false;
                 st.pop();
             }
         }
-        return st.empty();
+        return st.empty();  
     }
 };
