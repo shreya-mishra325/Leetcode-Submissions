6abdf5326f8a1f1ab3a267bd933bec0b363e186a
Shreya Mishra <shreyamishra2k05@gmail.com>
2026-07-24 23:42:17 +0530

    Time: 14 ms (22.54%), Space: 7.7 MB (65.45%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,15 +1,8 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n==0) return 0;
         if(n==1) return 1;
-        int ans;
-        int curr=1, prev=0;
-        for(int i=2; i<=n; i++){
-            ans=curr+prev;
-            prev=curr;
-            curr=ans;
-        }  
-        return ans;    
+        if(n==0) return 0;
+        return fib(n-1)+fib(n-2);
     }
 };
