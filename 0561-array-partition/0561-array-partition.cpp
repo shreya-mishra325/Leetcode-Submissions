0fd4f5160f49688b702f07a812e8140aeaa9a986
Shreya Mishra <shreyamishra2k05@gmail.com>
2026-08-08 01:50:07 +0530

    Time: 15 ms (32.37%), Space: 32.1 MB (22.92%) - LeetHub

---
+++ 0561-array-partition/0561-array-partition.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int arrayPairSum(vector<int>& nums) {
+        int ans=0;
+        sort(nums.begin(), nums.end());
+        for(int i=0; i<nums.size(); i=i+2){
+            int j=i+1;
+            ans=ans+min(nums[i], nums[j]);
+        }
+        return ans;
+    }
+};
