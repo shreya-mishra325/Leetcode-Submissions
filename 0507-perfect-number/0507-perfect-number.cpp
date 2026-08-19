7abf40b52bc5d162f9456c4be3a873df4129947c
Shreya Mishra <shreyamishra2k05@gmail.com>
2026-08-20 02:26:35 +0530

    Time: 0 ms (100%), Space: 7.7 MB (55.43%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.cpp
+++ 0507-perfect-number/0507-perfect-number.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     bool checkPerfectNumber(int num) {
-        int sum=0;
-        for(int i=1; i<num; i++){
-            if(num%i==0){
-                sum=sum+i;
-            }
+       if(num<=1) return false;
+       int sum=1;
+       for(int i=2; i*i<=num; i++){
+        if(num%i==0) { sum=sum+i;
+        if(i*i!=num){
+            sum=sum+(num/i);
         }
-        if(sum==num) return true;
-        return false;
+       }
+    }
+    return sum==num;
     }
 };
