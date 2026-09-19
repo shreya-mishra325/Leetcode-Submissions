e12411a8114f1131f6e6870add482d01ea476471
Shreya Mishra <shreyamishra2k05@gmail.com>
2026-09-20 01:29:05 +0530

    Time: 0 ms (100%), Space: 10.5 MB (30.38%) - LeetHub

---
+++ 0812-largest-triangle-area/0812-largest-triangle-area.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    double largestTriangleArea(vector<vector<int>>& points) {
+        double ans=0;
+        for(int i=0; i<points.size(); i++){
+            for(int j=i+1; j<points.size(); j++){
+                for(int k=j+1; k<points.size(); k++){
+                    int x1=points[i][0];
+                    int y1=points[i][1];
+
+                    int x2=points[j][0];
+                    int y2=points[j][1];
+
+                    int x3=points[k][0];
+                    int y3=points[k][1];
+
+                    double area=0.5*abs(x1*(y2-y3) + x2*(y3-y1) + x3*(y1-y2));
+                    ans=max(ans,area);
+                }
+            }
+        }
+        return ans;
+    }
+};
