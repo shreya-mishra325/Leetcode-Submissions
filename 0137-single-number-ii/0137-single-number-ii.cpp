728b9ea49875dffb7b765a2510c104a1e80f0863
Shreya Mishra <shreyamishra2k05@gmail.com>
2026-09-26 18:02:53 +0530

    Time: 4 ms (14.68%), Space: 14.2 MB (11.25%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        unordered_map<int, int> freq;
+        for(int i=0; i<nums.size(); i++){
+            freq[nums[i]]++;
+        }
+        for(int i=0; i<nums.size(); i++){
+            if(freq[nums[i]]<3) return nums[i];
+        }
+        return -1;
+    }
+};
